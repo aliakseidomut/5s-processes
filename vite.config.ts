0224433cc2bf84c451a683b85db8d00f5b674050
Aliaksei Domut <alexedomut@gmail.com>
2024-12-29 23:42:09 +0300

    fix: alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "../"),
-      "@components": path.resolve(__dirname, "../components"),
-      "@pages": path.resolve(__dirname, "../pages"),
-      "@utils": path.resolve(__dirname, "../utils"),
-      "@styles": path.resolve(__dirname, "../styles"),
-      "@assets": path.resolve(__dirname, "../assets"),
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@utils": "/src/utils",
+      "@styles": "/src/styles",
+      "@assets": "/src/assets",
     },
   },
 });
